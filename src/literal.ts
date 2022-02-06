d93b49994514ef01cfb9ff91ef0d3ff389e6df46
Noriyuki <decoy.nori@gmail.com>
2022-02-06 23:59:45 +0900

    literal.tsを追加

---
+++ src/literal.ts
@@ -0,0 +1,13 @@
+let dayOfTheWeek: string = '日';
+dayOfTheWeek = '月';
+
+let literalWeek: '月' | '火' | '水' | '木' | '金' | '土' | '日' = '日';
+literalWeek = '月';
+//literalWeek = '31';
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+month = 12;
+//month = 20;
+
+let myFlg: true | false;
+// myFlg = 1;
