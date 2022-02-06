a33a528320bc5891c02a63f4b46c37dd2cb87cf4
Noriyuki <decoy.nori@gmail.com>
2022-02-06 09:12:38 +0900

    TypeScriptのインストール

---
+++ src/install-typescript.ts
@@ -0,0 +1,19 @@
+let message: string = 'Hello,TypeScriot!';
+console.log({ message });
+
+//package.jsonを作成する
+// npm init -y
+
+//TypeScript
+//バージョン調査
+// npm info typescript
+//インストール
+// npm install --save-dev typescript@4.5.5
+//実行
+// ./node_modules/.bin/tsc src/install-typescript.ts
+// npx tsc src/install-typescript.ts
+// node src/install-typescript.js
+
+//バージョン調査
+//インストール
+//実行
