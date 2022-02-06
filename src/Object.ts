b3dfe47571770aa6c4c3497681350874d89f2c94
Noriyuki <decoy.nori@gmail.com>
2022-02-06 20:11:57 +0900

    object.tsを追加

---
+++ src/Object.ts
@@ -0,0 +1,10 @@
+console.log('Object.ts');
+
+let profile1: object = { name: 'ao' };
+profile1 = { birthYear: '2005' };
+
+let profile2: {
+	name: string;
+	birthYear: number;
+} = { name: 'Aka', birthYear: 2005 };
+// profile2 = { birthYear: 2005 };
