a365c02258e54244e868b908bb375f58a67ed112
Noriyuki <decoy.nori@gmail.com>
2022-02-07 08:44:41 +0900

    無名関数

--- src/function.ts
+++ src/function.ts
@@ -1,5 +1,11 @@
-const BMI = (height: number, weight: number): number => {
+//無名関数
+const BMIanonymous1 = function (height: number, weight: number): number {
 	return weight / (height * height);
 };
+console.log(BMIanonymous1(1.8, 73));
 
-console.log(BMI(1.8, 73));
+//アロー関数
+const BMIarrow = (height: number, weight: number): number => {
+	return weight / (height * height);
+};
+console.log(BMIarrow(1.8, 73));
